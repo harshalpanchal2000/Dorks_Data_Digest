51ed5a72e30d9d4a077b23099f00cb78c27f8ef6
Harshal Panchal <78840044+harshalpanchal2000@users.noreply.github.com>
2024-05-01 17:17:37 -0400

    Update app.py

--- app.py
+++ app.py
@@ -16,9 +16,9 @@ dfs = {
     "Machine Learning": machine_learning_df,
     "Deep Learning": deep_learning_df,
     "Computer Vision": computer_vision_df,
-    "NLP": nlp_df,
-    "AI": ai_df,
-    "Math": math_df
+    "Natural Language Processing": nlp_df,
+    "Artificial Intelligence": ai_df,
+    "Mathematics": math_df
 }
 
 # Set page title and page layout
@@ -26,7 +26,7 @@ st.set_page_config(page_title="Dork's Data Digest", page_icon=":books:", layout=
 
 # Define the UI layout
 st.markdown("<h1 style='text-align: center;'><span style='font-size:48px; display: inline-block;'>📚📚📚</span> <b style='display: inline-block;'>Dork's Data Digest</b></h1>", unsafe_allow_html=True)
-st.markdown("_Discover top-rated books based for Data Science_", unsafe_allow_html=True)
+st.markdown("<h3 style='text-align: center;'>Discover top-rated books based for Data Science</h3>", unsafe_allow_html=True)
 
 # Display buttons for each cluster in the main area
 col1, col2, col3 = st.columns(3)
@@ -36,15 +36,15 @@ cluster_emoticons = {
     "Machine Learning": "🤖",
     "Deep Learning": "🧠",
     "Computer Vision": "📷",
-    "NLP": "💬",
-    "AI": "🚀",
-    "Math": "🔢"
+    "Natural Language Processing": "💬",
+    "Artificial Intelligence": "🚀",
+    "Mathematics": "🔢"
 }
 for topic, df_cluster in dfs.items():
     if topic in ["Data Analysis", "Machine Learning", "Deep Learning"]:
         if col1.button(f"{cluster_emoticons[topic]} {topic}"):
             selected_cluster = topic
-    elif topic in ["Computer Vision", "NLP", "AI"]:
+    elif topic in ["Computer Vision", "Natural Language Processing", "Artificial Intelligence"]:
         if col2.button(f"{cluster_emoticons[topic]} {topic}"):
             selected_cluster = topic
     else:
