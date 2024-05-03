49c7a6327982a97ec8cdef6b7df0bc14a8c80601
Harshal Panchal <78840044+harshalpanchal2000@users.noreply.github.com>
2024-05-03 12:33:56 -0400

    Update data_analysis_page.py

--- pages/data_analysis_page.py
+++ pages/data_analysis_page.py
@@ -6,7 +6,7 @@ def data_analysis_page():
     st.subheader("Discover top-rated books for Data Analysis")
     
     # Load data
-    data_analysis_books_df = pd.read_csv('datasets/Data Analysis Books.csv')
+    data_analysis_books_df = pd.read_csv('datasets/Data Analysis.csv')
     
     # Display data
     st.write(data_analysis_books_df)
