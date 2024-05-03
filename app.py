d844c8c75af9d59f0f2a250100ac807769366c4f
Harshal Panchal <78840044+harshalpanchal2000@users.noreply.github.com>
2024-05-03 13:15:45 -0400

    Update app.py

--- app.py
+++ app.py
@@ -60,14 +60,17 @@ def display_homepage():
         else:
             button_col = col3
         
-        if button_col.button(f"{icon} {cluster}", key=f"{cluster}_button"):
-            display_page(cluster)
+        with button_col:
+            st.write("   ")  # Add left padding
+            if button_col.button(f"{icon} {cluster}", key=f"{cluster}_button"):
+                display_page(cluster)
 
     st.markdown(
-        "<div style='position: fixed; bottom: 20px; width: 100%; text-align: left; padding-left: 39%;'>"
+        "<div style='position: fixed; bottom: 20px; width: 100%; text-align: left; padding-left: 5%;'>"
         "<p>Built by <a href='https://www.linkedin.com/in/harshal-panchal/' target='_blank'>Harshal Panchal</a></p>"
         "</div>",
-        unsafe_allow_html=True)
+        unsafe_allow_html=True
+    )
 
 # Run the app
 def main():
