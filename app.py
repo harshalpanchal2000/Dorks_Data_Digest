854cc39ac3d08fef8a92468af84818e5f1d33863
Harshal Panchal <78840044+harshalpanchal2000@users.noreply.github.com>
2024-05-03 13:27:00 -0400

    Update app.py

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def display_homepage():
         ("Mathematics", "🧮")
     ]
 
-    col1, col2, col3 = st.beta_columns(3)
+    col1, col2, col3 = st.columns(3)
 
     for i, (cluster, icon) in enumerate(clusters):
         if i % 3 == 0:
@@ -60,13 +60,12 @@ def display_homepage():
         else:
             button_col = col3
         
-        if button_col.button(f"{icon} {cluster}", key=f"{cluster}_button"):
-            st.markdown("---")  # Add a horizontal line
-            display_page(cluster)
-            st.markdown("---")  # Add a horizontal line
+        with button_col:
+            if button_col.button(f"{icon} {cluster}", key=f"{cluster}_button"):
+                display_page(cluster)
 
     st.markdown(
-        "<div style='position: fixed; bottom: 20px; width: 100%; text-align: left; padding-left: 40%;'>"
+        "<div style='position: fixed; bottom: 20px; width: 100%; text-align: left; padding-left: 5%;'>"
         "<p>Built by <a href='https://www.linkedin.com/in/harshal-panchal/' target='_blank'>Harshal Panchal</a></p>"
         "</div>",
         unsafe_allow_html=True
