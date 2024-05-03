b7a1b3a1614a70d45a4db6bf0879a46de253a1c7
Harshal Panchal <78840044+harshalpanchal2000@users.noreply.github.com>
2024-05-03 12:07:04 -0400

    Create deep_learning_page.py

---
+++ pages/deep_learning_page.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+
+def deep_learning_page():
+    st.title("Deep Learning Books")
+    st.subheader("Discover top-rated books for Deep Learning")
+    
+    # Load data
+    deep_learning_books_df = pd.read_csv('datasets/Deep Learning Books.csv')
+    
+    # Display data
+    st.write(deep_learning_books_df)
+
+if __name__ == "__main__":
+    deep_learning_page()
