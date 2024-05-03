dc4775aad8f28f81fc6fd45b8aaba1e4ef26cefc
Harshal Panchal <78840044+harshalpanchal2000@users.noreply.github.com>
2024-05-03 12:07:53 -0400

    Update data_analysis_page.py

--- pages/data_analysis_page.py
+++ pages/data_analysis_page.py
@@ -2,9 +2,11 @@ import streamlit as st
 import pandas as pd
 
 def data_analysis_page():
-    st.title("Data Analysis Cluster Page")
-    data_analysis_df = pd.read_csv('datasets/Data Analysis.csv')
-    st.write(data_analysis_df)
-
-if __name__ == "__main__":
-    data_analysis_page()
+    st.title("Data Analysis Books")
+    st.subheader("Discover top-rated books for Data Analysis")
+    
+    # Load data
+    data_analysis_books_df = pd.read_csv('datasets/Data Analysis Books.csv')
+    
+    # Display data
+    st.write(data_analysis_books_df)
