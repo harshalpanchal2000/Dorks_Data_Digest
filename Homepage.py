e5991f30a9f0f75a0cdfcfe4c3221600221337b2
Harshal Panchal <78840044+harshalpanchal2000@users.noreply.github.com>
2024-05-06 15:20:56 -0400

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -32,7 +32,7 @@ def display_page(page):
     elif page == "Artificial Intelligence":
         ai_page()
     elif page == "Mathematics":
-        math_page.py
+        math_page()
 
 # Define the homepage layout
 def display_homepage():
