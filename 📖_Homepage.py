f0f7035551436a29cf55a6872f88530c23f12a0f
Harshal Panchal <78840044+harshalpanchal2000@users.noreply.github.com>
2024-05-06 15:58:18 -0400

    Update 📖_Homepage.py

--- 📖_Homepage.py
+++ 📖_Homepage.py
@@ -17,7 +17,7 @@ def display_homepage():
     )
 
     st.markdown(
-        "<div style='position: fixed; bottom: 20px; width: 100%; text-align: left; padding-left: 10%;'>"
+        "<div style='position: fixed; bottom: 20px; width: 100%; text-align: left; padding-left: 15%;'>"
         "<p>Built by <a href='https://www.linkedin.com/in/harshal-panchal/' target='_blank'>Harshal Panchal</a></p>"
         "</div>",
         unsafe_allow_html=True
